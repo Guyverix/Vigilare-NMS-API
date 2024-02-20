206a464354489f3fc2b180a876b26b7329027941
Christopher Hubbard <chubbard@iwillfearnoevil.com>
2024-02-20 22:05:46 +0000

    alter script control for debugger file and stdout to be a little more user friendly

--- templates/readDebuggerFile.php
+++ templates/readDebuggerFile.php
@@ -1,4 +1,4 @@
-#!/usr/bin/php
+#!/usr/bin/env php
 <?php
 /*
   Just a generic utility that converts the stored JSON file back
@@ -20,16 +20,14 @@ if ( ! file_exists($fileName)) {
   exit();
 }
 
-echo "Pulling from file and converting back from JSON to array output\n";
+echo "Pulling from file and converting back from JSON to array output:\n";
 $allDataConvert = file_get_contents("$fileName");
-
-
 $allDataConvert = json_decode($allDataConvert,1);
 print_r($allDataConvert);
 echo "\n\n";
 
 $insert = json_encode($allDataConvert['dataToBeInserted'],1);
-echo "Insert data as JSON\n";
+echo "Insert data as JSON:\n";
 echo $insert;
 
 echo "\n\n";
@@ -37,14 +35,15 @@ print_r($allDataConvert['dataToBeInserted']);
 
 $test=json_decode($allDataConvert['dataToBeInserted'], true);
 
-echo "\nFOREACH\n";
+echo "\nFOREACH Loop of data:\n";
 //foreach ($allDataConvert['dataToBeInserted'] as $k => $v) {
 foreach ($test as $k => $v) {
   echo "Key " . $k . " Value " . $v . "\n";
 }
 
 $jsonData=json_encode($test,1); 
-echo "JSON dataToBeInserted\n\n";
+echo "JSON dataToBeInserted:\n\n";
 echo $jsonData;
 
+echo "\n";
 ?>
